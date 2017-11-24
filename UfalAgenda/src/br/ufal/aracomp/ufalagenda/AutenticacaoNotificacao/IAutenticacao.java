5adc9bbf9cb0128d962d6cd90bb5ecd4a0002e09
João Batista Gomes Silva <jhonxbatista@gmail.com>
2017-11-24 08:22:03 -0300

    Definição das assinaturas dos métodos para as interfaces de Notificação e Autenticação

--- UfalAgenda/src/br/ufal/aracomp/ufalagenda/AutenticacaoNotificacao/IAutenticacao.java
+++ UfalAgenda/src/br/ufal/aracomp/ufalagenda/AutenticacaoNotificacao/IAutenticacao.java
@@ -1,5 +1,30 @@
 package br.ufal.aracomp.ufalagenda.AutenticacaoNotificacao;
 
-public interface IAutenticacao {
+import br.ufal.aracomp.ufalagenda.persistencia.metamodel.Usuario;
 
+public interface IAutenticacao {
+	
+	/*
+	 * Verifica se o usuário é válido e tem como retorno true ou false
+	 * aut_tipo = Campo de autenticação, como email, login, facebook
+	 * aut_valor = Valor do campo de autenticação
+	 * senha = Senha do usuário
+	 * */
+	
+	public boolean autenticar(String aut_tipo, String aut_valor, String senha);
+	
+	/*
+	 * Verifica se o usuário é válido e tem como retorno true ou false
+	 * aut_valor = Valor do campo de autenticação
+	 * senha = Senha do usuário
+	 * */
+	public boolean autenticar(String aut_valor, String senha);
+	
+	/*
+	 * Trata os dados do usuário e verifica se o mesmo está autenticado como retorno true ou false
+	 * usuario = Objeto do tipo Usuario
+	 * */
+	public boolean logar(Usuario usuario);
+	
+	
 }
