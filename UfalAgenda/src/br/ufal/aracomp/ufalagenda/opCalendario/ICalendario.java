e77682dd69a0913b3c0fdcb9b8d3badfd8e3bd6a
Jefferson Farias <Jefferson Farias@DESKTOP-5MSE7OA>
2017-11-25 20:38:54 -0200

    Criou as assinaturas dos métodos da interface ICalendario.

--- UfalAgenda/src/br/ufal/aracomp/ufalagenda/opCalendario/ICalendario.java
+++ UfalAgenda/src/br/ufal/aracomp/ufalagenda/opCalendario/ICalendario.java
@@ -1,5 +1,27 @@
 package br.ufal.aracomp.ufalagenda.opCalendario;
 
+import java.util.Date;
+import java.util.List;
+
+import br.ufal.aracomp.ufalagenda.persistencia.metamodel.Agenda;
+import br.ufal.aracomp.ufalagenda.persistencia.metamodel.Agendamento;
+import br.ufal.aracomp.ufalagenda.persistencia.metamodel.Definido;
+import br.ufal.aracomp.ufalagenda.persistencia.metamodel.Usuario;
+
 public interface ICalendario {
+	
+	public List <Agendamento> acessarCalendario(Date inicio, Date fim, Agenda agenda);
+	
+	public boolean criarEvento(Definido definido);
+	
+	public boolean editarEvento(Definido agendamento); 
+	
+	public boolean excluirEvento(int idAgendamento);
+	
+	public boolean confirmarPresenca(Usuario usuario, Definido definido);
+	
+	public boolean compartilharEvento(Agendamento agendamento, List<Usuario> usuarios);
+	
+	
 
 }
